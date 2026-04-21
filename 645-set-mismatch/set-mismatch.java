d5010c247f3dc270b6c3b25c7493ded14642ec1e
harshagrawal1003 <vindeshwariagrawal1234@gmail.com>
2026-04-21 07:20:26 +0530

    Time: 4 ms (45.10%) | Memory: 47.5 MB (68.69%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] findErrorNums(int[] arr) {
+        int i = 0;
+
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+
+            if (arr[i] <= arr.length && arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+        for(int j=0;j<arr.length;j++)
+        {
+            if(j+1!=arr[j])
+            return new int[]{arr[j],j+1};
+        } 
+         return new int[]{-1,-1};
+    }
+          static void swap(int[]arr,int a,int b)
+    {
+        int temp=arr[a];
+        arr[a]=arr[b];
+        arr[b]=temp;
+    }
+}
