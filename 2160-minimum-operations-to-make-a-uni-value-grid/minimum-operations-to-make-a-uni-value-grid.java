684a093363809150dffd5c2a805c7d86854895b3
harshagrawal1003 <vindeshwariagrawal1234@gmail.com>
2026-04-28 07:12:57 +0530

    Time: 103 ms (39.69%) | Memory: 104 MB (99.24%) - LeetSync

---
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minOperations(int[][] grid, int x) {
+        List<Integer> list = new ArrayList<>();
+        for (int[] row : grid) {
+            for (int num : row) {
+                list.add(num);
+            }
+        }
+        int base = list.get(0);
+        for (int num : list) {
+            if ((num - base) % x != 0) {
+                return -1;
+            }
+        }
+        Collections.sort(list);
+        int median = list.get(list.size() / 2);
+        int operations = 0;
+        for (int num : list) {
+            operations += Math.abs(num - median) / x;
+        }
+        return operations;
+    }
+}
