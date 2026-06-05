580d00b050494b396df0df6e7a71d7009841bed3
harshagrawal1003 <vindeshwariagrawal1234@gmail.com>
2026-06-05 19:42:26 +0530

    Time: 1 ms (99.96%) | Memory: 46.6 MB (90.39%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,10 +1,9 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        int sum=0;
-        for(int i:nums)
-        {
-            sum^=i;
+    public int singleNumber(int[] arr) {
+         int no=0;
+        for (int i = 0; i < arr.length; i++) {
+            no^=arr[i];
         }
-        return sum;
+        return no;
     }
 }
