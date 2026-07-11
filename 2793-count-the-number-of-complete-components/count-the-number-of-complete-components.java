51c12b078fd5c92f225d234b71a921a8f3db739e
harshagrawal1003 <vindeshwariagrawal1234@gmail.com>
2026-07-11 21:42:15 +0530

    Time: 11 ms (59.60%) | Memory: 47.8 MB (20.28%) - LeetSync

---
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int countCompleteComponents(int n, int[][] edges) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+
+        boolean[] visited = new boolean[n];
+        int completeComponentsCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                int[] counts = new int[2];
+                dfs(i, adj, visited, counts);
+                
+                int nodeCount = counts[0];
+                int edgeCount = counts[1] / 2;
+
+                if (edgeCount == (nodeCount * (nodeCount - 1)) / 2) {
+                    completeComponentsCount++;
+                }
+            }
+        }
+
+        return completeComponentsCount;
+    }
+
+    private void dfs(int u, List<List<Integer>> adj, boolean[] visited, int[] counts) {
+        visited[u] = true;
+        counts[0]++;
+        counts[1] += adj.get(u).size();
+
+        for (int v : adj.get(u)) {
+            if (!visited[v]) {
+                dfs(v, adj, visited, counts);
+            }
+        }
+    }
+}
